e64ce643924abdedd0cdfde7953b9b4defad4f61
Mykhailo Kutsybala <mkutsybala@gmail.com>
2023-03-06 12:55:11 +0200

    tfstate moved to backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+   bucket = "mk-s3-bucket-for-back-end"
+   region = "eu-west-2"
+   key = "infrastructure/terraform.tfstate"
+   dynamodb_table = "terraform-lock"
+   encrypt = true
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
